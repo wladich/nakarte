ce87f6507fa6410fc7bee7a57d296840b6c8bce7
Sergej Orlov <wladimirych@gmail.com>
2017-02-09 11:34:37 +0300

    [knockout progress component] fixed typo

--- src/lib/knockout.component.progress/progress.js
+++ src/lib/knockout.component.progress/progress.js
@@ -5,7 +5,7 @@ ko.components.register('progress-indicator', {
     template:   `
         <div class="ko-progress-unknown" data-bind="visible: progress() === undefined"></div>
         <div class="ko-progress" data-bind="visible: progress() !== undefined">
-            <div class="ko-progress-bkg">' +
+            <div class="ko-progress-bkg">
                 <div class="ko-progress-bar"  data-bind="style: {width: progress() + '%'}"></div>
             </div>
         </div>`,
