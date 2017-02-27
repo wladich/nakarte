90cf5f80eb2776c03331dae83d948c7e9dc9e115
Sergej Orlov <wladimirych@gmail.com>
2017-02-27 22:09:21 +0300

    [build] cleanup babel plugins and presets config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -46,7 +46,6 @@ module.exports = {
     // require.resolve('webpack/hot/dev-server'),
     require.resolve('react-dev-utils/webpackHotDevClient'),
     // We ship a few polyfills by default:
-    require.resolve('./polyfills'),
     // Finally, this is your app's code:
     paths.appIndexJs
     // We include the app code last so that if there is a runtime error during
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -55,7 +55,6 @@ module.exports = {
   // In production, we only want to load the polyfills and the app code.
   entry: [
     'babel-polyfill',
-    require.resolve('./polyfills'),
     paths.appIndexJs
   ],
   output: {
