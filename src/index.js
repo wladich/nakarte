f977d16f60ecc780b6b455218e77c6e9b49c1de0
Sergej Orlov <wladimirych@gmail.com>
2017-03-06 22:22:36 +0300

    made promises rejection  tracking compatible with polyfill from core-js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import App from './App'
 import config from './config';
 
 Raven.config(config.sentryDSN).install();
-window.addEventListener('unhandledrejection', (e) => {
+
+const oldOnunhandledrejection = window.onunhandledrejection;
+
+// Not using addEventListener due to https://github.com/zloirock/core-js/issues/205
+window.onunhandledrejection = (e) => {
+    let result = true;
+    if (oldOnunhandledrejection) {
+         result = oldOnunhandledrejection(e);
+    }
+    console.error('Uncaught in promise:', e.reason);
     const err = e.reason;
     if (err.name) {
         err.name = 'Uncaught in promise: ' + err.name;
     }
     Raven.captureException(err);
-});
+    return result;
+};
 
 App.setUp();
 
