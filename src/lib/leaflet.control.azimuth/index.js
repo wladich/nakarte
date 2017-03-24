2e46a04dfdd3bc26c4fd6f48dc159e30a62d006b
Sergej Orlov <wladimirych@gmail.com>
2017-03-25 01:15:22 +0300

    imported rotated-marker plugin to vendor dir

--- src/lib/leaflet.control.azimuth/index.js
+++ src/lib/leaflet.control.azimuth/index.js
@@ -5,8 +5,7 @@ import layout from './control.html';
 import 'lib/controls-styles/controls-styles.css';
 import './style.css';
 import {getDeclination} from 'lib/magnetic-declination';
-//FIXME: replace with vendored version
-import 'leaflet-rotatedmarker';
+import 'vendored/github.com/bbecquet/Leaflet.RotatedMarker/leaflet.rotatedMarker';
 import iconPointer from './pointer.svg';
 import iconPointerStart from './pointer-start.svg';
 import iconPointerEnd from './pointer-end.svg';
