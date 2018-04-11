fae9fe35270d7b9c79ff6ffc059abaa83e5e9d90
Sergej Orlov <wladimirych@gmail.com>
2018-04-11 11:32:53 +0300

    westrapasses: download via https

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import secrets from './secrets';
 export default Object.assign({
     email: 'nakarte@nakarte.tk',
     googleApiUrl: 'https://maps.googleapis.com/maps/api/js?v=3',
-    westraDataBaseUrl: 'http://nakarte.tk/westraPasses/',
+    westraDataBaseUrl: 'https://nakarte.tk/westraPasses/',
     CORSProxyUrl: 'http://proxy.nakarte.tk/',
     elevationsServer: 'http://elevation.nakarte.tk/',
     newsUrl: 'http://about.nakarte.tk',
