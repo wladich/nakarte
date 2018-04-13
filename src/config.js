d646e74f9453481aa9e9a560883c19a87bd546c0
Sergej Orlov <wladimirych@gmail.com>
2018-04-13 23:03:19 +0300

    elevation and tracks servers: use https

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ export default Object.assign({
     googleApiUrl: 'https://maps.googleapis.com/maps/api/js?v=3',
     westraDataBaseUrl: 'https://nakarte.tk/westraPasses/',
     CORSProxyUrl: 'https://proxy.nakarte.tk/',
-    elevationsServer: 'http://elevation.nakarte.tk/',
+    elevationsServer: 'https://elevation.nakarte.tk/',
     newsUrl: 'http://about.nakarte.tk',
     wikimediaCommonsCoverageUrl: 'https://tiles.nakarte.tk/wikimedia_commons_images/{z}/{x}/{y}',
     geocachingSuUrl: 'https://nakarte.tk/geocachingSu/geocaching_su.json',
-    tracksStorageServer: 'http://tracks.nakarte.tk',
+    tracksStorageServer: 'https://tracks.nakarte.tk',
 }, secrets);
