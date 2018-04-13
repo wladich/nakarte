a85df227dd4796dfe4d7a59124b530e779b71171
Sergej Orlov <wladimirych@gmail.com>
2018-04-13 22:52:37 +0300

    cors proxy: use https

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export default Object.assign({
     email: 'nakarte@nakarte.tk',
     googleApiUrl: 'https://maps.googleapis.com/maps/api/js?v=3',
     westraDataBaseUrl: 'https://nakarte.tk/westraPasses/',
-    CORSProxyUrl: 'http://proxy.nakarte.tk/',
+    CORSProxyUrl: 'https://proxy.nakarte.tk/',
     elevationsServer: 'http://elevation.nakarte.tk/',
     newsUrl: 'http://about.nakarte.tk',
     wikimediaCommonsCoverageUrl: 'https://tiles.nakarte.tk/wikimedia_commons_images/{z}/{x}/{y}',
