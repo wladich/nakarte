f9c727739427d8c0cad431cfb957df3cb7bf0568
Sergej Orlov <wladimirych@gmail.com>
2018-04-13 23:00:00 +0300

    bing dates: use https

--- src/lib/leaflet.layer.bing/dates.js
+++ src/lib/leaflet.layer.bing/dates.js
@@ -2,7 +2,7 @@ import L from 'leaflet';
 import {tile2quad} from './index';
 
 const BingDates = L.TileLayer.extend({
-        _url: 'http://mvexel.dev.openstreetmap.org/bingimageanalyzer/tile.php?t={quad}&nodepth=1',
+        _url: 'https://mvexel.dev.openstreetmap.org/bingimageanalyzer/tile.php?t={quad}&nodepth=1',
 
         initialize: function(options) {
             L.Util.setOptions(this, options);
