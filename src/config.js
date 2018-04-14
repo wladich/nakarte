dce3d6b2fa43023c3805e45d225e30916d2ab1ce
Sergej Orlov <wladimirych@gmail.com>
2018-04-14 11:43:29 +0300

    use https in link to blog

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export default Object.assign({
     westraDataBaseUrl: 'https://nakarte.tk/westraPasses/',
     CORSProxyUrl: 'https://proxy.nakarte.tk/',
     elevationsServer: 'https://elevation.nakarte.tk/',
-    newsUrl: 'http://about.nakarte.tk',
+    newsUrl: 'https://about.nakarte.tk',
     wikimediaCommonsCoverageUrl: 'https://tiles.nakarte.tk/wikimedia_commons_images/{z}/{x}/{y}',
     geocachingSuUrl: 'https://nakarte.tk/geocachingSu/geocaching_su.json',
     tracksStorageServer: 'https://tracks.nakarte.tk',
