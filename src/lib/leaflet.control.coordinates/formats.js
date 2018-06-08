5162bd8e86656fb1c90e35892c0af59a4a89b219
Igor Sidorov <igor.sidorov@onapp.com>
2018-06-08 08:26:10 +0300

    More compact UI for coordinates

--- src/lib/leaflet.control.coordinates/formats.js
+++ src/lib/leaflet.control.coordinates/formats.js
@@ -43,6 +43,7 @@ function transform(latlng, format) {
 const SIGNED_DEGREES = {
     code: 'd',
     label: '±ddd.ddddd',
+    wrapperClass: 'leaflet-coordinates-wrapper-signed-degrees',
     process: (latlng) => {
         return transform(latlng, ({signedDegrees}) => signedDegrees);
     }
@@ -51,6 +52,7 @@ const SIGNED_DEGREES = {
 const DEGREES = {
     code: 'D',
     label: 'ddd.ddddd°',
+    wrapperClass: 'leaflet-coordinates-wrapper-degrees',
     process: (latlng) => {
         return transform(latlng, ({degrees, direction}) => {
             return `${direction} ${degrees}°`;
@@ -61,6 +63,7 @@ const DEGREES = {
 const DEGREES_AND_MINUTES = {
     code: 'DM',
     label: 'ddd°mm.mmm′',
+    wrapperClass: 'leaflet-coordinates-wrapper-degrees-and-minutes',
     process: (latlng) => {
         return transform(latlng, ({intDegrees, minutes, direction}) => {
             return `${direction} ${intDegrees}°${minutes}′`;
@@ -71,6 +74,7 @@ const DEGREES_AND_MINUTES = {
 const DEGREES_AND_MINUTES_AND_SECONDS = {
     code: 'DMS',
     label: 'ddd°mm′ss.s″',
+    wrapperClass: 'leaflet-coordinates-wrapper-degrees-and-minutes-and-seconds',
     process: (latlng) => {
         return transform(latlng, ({intDegrees, intMinutes, seconds, direction}) => {
             return `${direction} ${intDegrees}°${intMinutes}′${seconds}″`;
