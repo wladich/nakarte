3242f42099ca6ff9e75e10b5cb8fbcf184b6da13
Sergej Orlov <wladimirych@gmail.com>
2018-07-18 17:55:59 +0300

    [layers] changed group, added description for finland topo #109

--- src/layers.js
+++ src/layers.js
@@ -907,6 +907,7 @@ export default function getLayers() {
                 },
 		        {
 		            title: 'Finland Topo',
+                    description: '<a href="https://www.retkikartta.fi/?lang=en">https://www.retkikartta.fi/</a>',
 		            isDefault: false,
 		            layer: L.tileLayer("https://retkikartta.fi/wmts/30c616a00f157e7357721900e8b0415c?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=maastokartta&STYLE=default&TILEMATRIXSET=WGS84_Pseudo-Mercator&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image/png",
 		                {
@@ -999,7 +1000,8 @@ export default function getLayers() {
                 'Caucasus 1km',
                 'Caucasus 500m',
                 'Topo 250m',
-                'Montenegro topo 250m'],
+                'Montenegro topo 250m',
+                'Finland Topo'],
         },
         {
             title: 'Miscellaneous',
@@ -1044,11 +1046,6 @@ export default function getLayers() {
                 'Czech winter',
                 'Czech geographical'],
 
-        },
-        {
-            title: 'Finland',
-            layers: [
-                'Finland Topo'],
         }];
 
 
