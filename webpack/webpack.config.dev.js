19a6526a512fb301ac756d0d099e1d24312292c6
Sergej Orlov <wladimirych@gmail.com>
2019-12-01 21:18:10 +0100

    config: separate local imports with newline

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,5 +1,6 @@
 const Webpack = require('webpack');
 const merge = require('webpack-merge');
+
 const paths = require('./paths');
 const common = require('./webpack.common.js');
 
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,6 +1,7 @@
 const Webpack = require('webpack');
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const common = require('./webpack.common.js');
 const paths = require('./paths');
 
