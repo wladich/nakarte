0c6e93716b7747b5cf7036b08b805cb79a517533
Sergej Orlov <wladimirych@gmail.com>
2019-12-21 21:16:18 +0300

    tests: remove dummy test

--- test/test.js
+++
@@ -1,9 +0,0 @@
-import Endomodo from '../src/lib/leaflet.control.track-list/lib/services/endomondo.js';
-
-describe('Array', () => {
-    describe('#indexOf()', () => {
-        it('should return -1 when the value is not present', () => {
-            assert.equal(-1, [1,2,3].indexOf(4));
-        });
-    });
-});
