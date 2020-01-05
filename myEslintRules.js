420c41420c1ee716310fea8bbdad2562149c6270
Sergej Orlov <wladimirych@gmail.com>
2020-01-05 10:01:57 +0100

    eslint: move common rules to separate rules file

---
+++ myEslintRules.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ["eslint:recommended"],
+    rules: {
+        "semi": "error", // missing in eslint:recommended
+        "eol-last": "error", // missing in eslint:recommended
+    }
+};
