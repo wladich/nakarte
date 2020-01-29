18aa61e2e64a3e3c6eba1284a5b3f51cef014598
Nikolay Korotkiy <sikmir@gmail.com>
2020-01-29 10:46:17 +0300

    move defaultLocation and defaultZoom to config

--- src/config.js
+++ src/config.js
@@ -5,6 +5,8 @@ export default {
         <a href="https://docs.nakarte.me">Documentation</a> |
         <a href="https://about.nakarte.me">News</a> |
         <a href="mailto:nakarte@nakarte.me" target="_self">nakarte@nakarte.me</a> `,
+    defaultLocation: [55.75185, 37.61856],
+    defaultZoom: 10,
     googleApiUrl: `https://maps.googleapis.com/maps/api/js?v=3&key=${secrets.google}`,
     westraDataBaseUrl: 'https://nakarte.me/westraPasses/',
     CORSProxyUrl: 'https://proxy.nakarte.me/',
