358bc4daa2795b6da7d6e46356e52f02a9b402df
Sergej Orlov <wladimirych@gmail.com>
2020-02-06 22:15:09 +0100

    config: enable optional chaining and nullish coalescing

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -53,7 +53,12 @@ const babelConfig = {
         },
     ],
 
-    plugins: ['@babel/plugin-syntax-dynamic-import', '@babel/plugin-proposal-class-properties'],
+    plugins: [
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-optional-chaining',
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+    ],
 };
 
 const sourceMapOption = {
