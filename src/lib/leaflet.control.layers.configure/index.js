a75f4ecf1c0a4cf7624f8721a8b4ab969fc82e85
Sergey Orlov <wladimirych@gmail.com>
2020-04-11 11:47:59 +0200

    custom layers: limit custom layer name length to 40 characters. #362

--- src/lib/leaflet.control.layers.configure/index.js
+++ src/lib/leaflet.control.layers.configure/index.js
@@ -292,7 +292,9 @@ function enableConfig(control, {layers, customLayersOrder}) {
 /* eslint-disable max-len */
                 const formHtml = `
 <p><a class="doc-link" href="https://leafletjs.com/reference-1.0.3.html#tilelayer" target="_blank">See Leaflet TileLayer documentation for url format</a></p>
-<label>Layer name<br/><input data-bind="value: name"/></label><br/>
+<label>Layer name<br/>
+<span class="hint">Maximum 40 characters</span><br/>
+<input maxlength="40" style="width: 100%" data-bind="value: name"/></label><br/>
 <label>Tile url template<br/><textarea data-bind="value: url" style="width: 100%"></textarea></label><br/>
 <label><input type="radio" name="overlay" data-bind="checked: isOverlay, checkedValue: false">Base layer</label><br/>
 <label><input type="radio" name="overlay" data-bind="checked: isOverlay, checkedValue: true">Overlay</label><br/>
