e04e4d6655a456050515e5d99387879f796ff672
myadzel <myadzel@gmail.com>
2020-06-18 12:00:57 +0300

    parse garmin connect routes and activities #398

--- src/lib/leaflet.control.track-list/lib/services/index.js
+++ src/lib/leaflet.control.track-list/lib/services/index.js
@@ -9,6 +9,7 @@ import Tracedetrail from './tracedetrail';
 import {YandexRuler} from './yandex';
 import {NakarteTrack, NakarteUrl} from './nakarte';
 import {MovescountMove, MovescountRoute} from './movescount';
+import {GarminActivity, GarminRoute} from './garmin';
 import {SportsTrackerActivity} from './sportstracker';
 
 export default [
@@ -24,6 +25,8 @@ export default [
     Tracedetrail,
     MovescountMove,
     MovescountRoute,
+    GarminActivity,
+    GarminRoute,
     SportsTrackerActivity,
     SimpleService
 ];
