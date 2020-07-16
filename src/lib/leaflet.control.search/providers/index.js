ac7971807ccd7d284b66f9a4e56441a85b4c8337
Sergey Orlov <wladimirych@gmail.com>
2020-07-16 17:22:35 +0200

    search: extract coordinates from query

--- src/lib/leaflet.control.search/providers/index.js
+++ src/lib/leaflet.control.search/providers/index.js
@@ -1,12 +1,13 @@
 import {MapyCzProvider} from './mapycz';
 import {PhotonProvider} from './photon';
 import {LinksProvider} from './links';
+import {CoordinatesProvider} from "./coordinates";
 
 const providers = {
     mapycz: MapyCzProvider,
     photon: PhotonProvider,
 };
 
-const magicProviders = [LinksProvider];
+const magicProviders = [LinksProvider, CoordinatesProvider];
 
 export {providers, magicProviders};
