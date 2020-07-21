75c532500b6b3da18e6cf3fc93c1b395db12127c
Sergey Orlov <wladimirych@gmail.com>
2020-07-21 21:49:42 +0200

    Add workaround for bug in Chrome on Android - remove focus from input after prompt
    There is a bug in Chrome on Android: after closing the prompt, it sets
    focus to last active input element.

--- src/lib/notifications/index.js
+++ src/lib/notifications/index.js
@@ -5,8 +5,18 @@ function notify(message, onOk) {
     alertify.alert(message, onOk);
 }
 
-function prompt(message, value) {
-    return window.prompt(message, value);
+function query(message, value) {
+    function removeFocusFromInput() {
+        const activeElement = document.activeElement;
+        if (activeElement?.tagName === 'INPUT') {
+            activeElement.blur();
+        }
+    }
+
+    const result = window.prompt(message, value);
+    // There is a bug in Chrome on Android: after closing the prompt, it sets focus to last active input element
+    setTimeout(removeFocusFromInput, 0);
+    return result;
 }
 
-export {notify, prompt};
+export {notify, query};
