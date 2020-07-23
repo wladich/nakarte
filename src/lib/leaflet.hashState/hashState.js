75dd05aa0b9d14bc58247fd8c848e0f0e946da7a
Sergey Orlov <wladimirych@gmail.com>
2020-07-23 09:40:16 +0200

    hash state: do not urldecode all values by default
    Decoding values broke loading state for new search control:
    nktl= part in search query caused track to be loaded.

--- src/lib/leaflet.hashState/hashState.js
+++ src/lib/leaflet.hashState/hashState.js
@@ -22,7 +22,6 @@ function parseHashParams(s) {
                 [, key, value] = m;
                 if (value) {
                     value = value.split('/');
-                    value = value.map(decodeURIComponent);
                 } else {
                     value = [];
                 }
