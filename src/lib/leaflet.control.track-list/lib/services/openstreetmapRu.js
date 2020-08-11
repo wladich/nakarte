db34415bc9cc0ab2a1dc3be522d6fbf9ae72d9fc
Sergey Orlov <wladimirych@gmail.com>
2020-08-11 16:19:54 +0200

    tracks: handle invalid links for openstreetmap.ru

--- src/lib/leaflet.control.track-list/lib/services/openstreetmapRu.js
+++ src/lib/leaflet.control.track-list/lib/services/openstreetmapRu.js
@@ -25,8 +25,14 @@ class OpenStreetMapRu extends BaseService {
     parseResponse(responses) {
         const trackId = this.getTrackId();
         const response = responses[0];
-        return parseGpx(response.responseBinaryText, `PersonalMap ${trackId}`, true) ||
-            [{name: name, error: 'UNSUPPORTED'}];
+        const geodata = parseGpx(response.responseBinaryText, `PersonalMap ${trackId}`, true);
+        if (!geodata) {
+            return [{name: name, error: 'UNSUPPORTED'}];
+        }
+        if (geodata[0].tracks.length === 0 && geodata[0].points.length === 0) {
+            return [{error: 'Personal map is empty or does not exist'}];
+        }
+        return geodata;
     }
 }
 
