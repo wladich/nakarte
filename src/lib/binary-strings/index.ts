6143d1e0b0f4c4ba6abaf05ad339cee2cfffa7dd
Sergey Orlov <wladimirych@gmail.com>
2020-09-16 08:57:42 +0200

    binary-string: add typescript annotations, slight refactoring

--- src/lib/binary-strings/index.ts
+++ src/lib/binary-strings/index.ts
@@ -1,27 +1,23 @@
-function arrayBufferToString(arBuf) {
-    const
-        arr = new Uint8Array(arBuf),
-        s = [];
+function arrayBufferToString(arBuf: ArrayBuffer): string {
+    const arr = new Uint8Array(arBuf);
+    const s = [];
     for (let i = 0; i < arr.length; i += 4096) {
-        let chunk = arr.subarray(i, i + 4096);
-        chunk = String.fromCharCode.apply(null, chunk);
-        s.push(chunk);
+        const chunk = arr.subarray(i, i + 4096);
+        s.push(String.fromCharCode.apply(null, (chunk as unknown) as Array<number>));
     }
     return s.join('');
 }
 
-function stringToArrayBuffer(s) {
-    const
-        length = s.length,
-        buf = new ArrayBuffer(length),
-        arr = new Uint8Array(buf);
-    for (let i = 0; i < length; i++) {
+function stringToArrayBuffer(s: string): ArrayBuffer {
+    const buf = new ArrayBuffer(s.length);
+    const arr = new Uint8Array(buf);
+    for (let i = 0; i < s.length; i++) {
         arr[i] = s.charCodeAt(i);
     }
     return buf;
 }
 
-function blobFromString(s) {
+function blobFromString(s: string): Blob {
     const arr = stringToArrayBuffer(s);
     return new Blob([arr]);
 }
