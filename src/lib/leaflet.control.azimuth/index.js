19c88c4f89841069089f04a04e57c47f4f4e8c21
Sergey Orlov <wladimirych@gmail.com>
2020-09-18 09:13:59 +0200

    replace rotated-marker in "vendored/" with npm package

--- src/lib/leaflet.control.azimuth/index.js
+++ src/lib/leaflet.control.azimuth/index.js
@@ -5,7 +5,7 @@ import layout from './control.html';
 import '~/lib/controls-styles/controls-styles.css';
 import './style.css';
 import {getDeclination} from '~/lib/magnetic-declination';
-import '~/vendored/github.com/bbecquet/Leaflet.RotatedMarker/leaflet.rotatedMarker';
+import 'leaflet-rotatedmarker';
 import iconPointer from './pointer.svg';
 import iconPointerStart from './pointer-start.svg';
 import iconPointerEnd from './pointer-end.svg';
