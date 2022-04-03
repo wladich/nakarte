617f50067ca7f684c4adfacc5dabf9672dd09cde
Sergej Orlov <wladimirych@gmail.com>
2022-04-03 22:45:18 +0200

    tracks: remove support for loading movescount moves and routes
    Movescount was closed

--- src/lib/leaflet.control.track-list/lib/services/index.js
+++ src/lib/leaflet.control.track-list/lib/services/index.js
@@ -6,7 +6,6 @@ import Strava from './strava';
 import Tracedetrail from './tracedetrail';
 import {YandexRuler} from './yandex';
 import {NakarteTrack, NakarteUrl} from './nakarte';
-import {MovescountMove, MovescountRoute} from './movescount';
 import {GarminActivity, GarminRoute} from './garmin';
 import {SportsTrackerActivity} from './sportstracker';
 import OpenStreetMapRu from './openstreetmapRu';
@@ -21,8 +20,6 @@ const services = [
     Osm,
     Strava,
     Tracedetrail,
-    MovescountMove,
-    MovescountRoute,
     GarminActivity,
     GarminRoute,
     SportsTrackerActivity,
