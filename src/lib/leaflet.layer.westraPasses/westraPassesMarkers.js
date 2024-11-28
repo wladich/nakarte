7761bae1d8b27a949788ddf4c7952287078ecb71
Sergej Orlov <wladimirych@gmail.com>
2024-11-28 22:55:21 +0100

    Add layer with FSTR passes

--- src/lib/leaflet.layer.westraPasses/westraPassesMarkers.js
+++ src/lib/leaflet.layer.westraPasses/westraPassesMarkers.js
@@ -1,90 +1,37 @@
-import L from 'leaflet';
-import '~/lib/leaflet.layer.canvasMarkers';
-import {openPopupWindow} from '~/lib/popup-window';
 import escapeHtml from 'escape-html';
+import L from 'leaflet';
+
+import './westraPassesMarkers.css';
+import {markerIcons, MountainPassesMarkers} from '~/lib/leaflet.layer.mountainPasses/mountainPassesMarkers';
+import {openPopupWindow} from '~/lib/popup-window';
 import {saveAs} from '~/vendored/github.com/eligrey/FileSaver';
-import {fetch} from '~/lib/xhr-promise';
-import {notify} from '~/lib/notifications';
-import * as logging from '~/lib/logging';
 
-import iconPass1a from './pass-1a.png';
-import iconPass1b from './pass-1b.png';
-import iconPass2a from './pass-2a.png';
-import iconPass2b from './pass-2b.png';
-import iconPass3a from './pass-3a.png';
-import iconPass3b from './pass-3b.png';
-import iconPassNoGrade from './pass-nograde.png';
-import iconPassUnknownGrade from './pass-unknown-notconfirmed.png';
-import iconSummit from './summit.png';
+const WestraPassesMarkers = MountainPassesMarkers.extend({
+    sourceName: 'Westra',
+    descriptionWidth: 500,
 
-const markerIcons = {
-    "1a": {url: iconPass1a, center: [7, 7]},
-    "1b": {url: iconPass1b, center: [7, 7]},
-    "2a": {url: iconPass2a, center: [7, 7]},
-    "2b": {url: iconPass2b, center: [7, 7]},
-    "3a": {url: iconPass3a, center: [7, 7]},
-    "3b": {url: iconPass3b, center: [7, 7]},
-    "nograde": {url: iconPassNoGrade, center: [7, 7]},
-    "unknown": {url: iconPassUnknownGrade, center: [7, 7]},
-    "summit": {url: iconSummit, center: [7, 7]},
-};
+    makeTooltip: function (marker) {
+        const properties = marker.properties;
+        let toolTip = properties.grade || '';
+        if (toolTip && properties.elevation) {
+            toolTip += ', ';
+        }
+        toolTip += properties.elevation || '';
+        if (toolTip) {
+            toolTip = ' (' + toolTip + ')';
+        }
+        toolTip = (properties.name || 'без названия') + toolTip;
+        toolTip = (properties.is_summit ? 'Вершина ' : 'Перевал ') + toolTip;
+        return toolTip;
+    },
 
-const WestraPassesMarkers = L.Layer.CanvasMarkers.extend({
-        options: {
-            filePasses: 'westra_passes.json',
-            scaleDependent: true
-        },
-
-        initialize: function(baseUrl, options) {
-            L.Layer.CanvasMarkers.prototype.initialize.call(this, null, options);
-            this.on('markerclick', this.showPassDescription, this);
-            this._baseUrl = baseUrl;
-            this.url = baseUrl + this.options.filePasses;
-        },
-
-        loadData: function() {
-            if (this._downloadStarted) {
-                return;
-            }
-            this._downloadStarted = true;
-            fetch(this.url)
-                .then(
-                    (xhr) => this._loadMarkers(xhr),
-                    (e) => {
-                        this._downloadStarted = false;
-                        logging.captureException(e, 'failed to get westra passes');
-                        notify('Failed to get Westra passes data');
-                    }
-                );
-        },
-
-        onAdd: function(map) {
-            L.Layer.CanvasMarkers.prototype.onAdd.call(this, map);
-            this.loadData();
-        },
-
-        _makeTooltip: function(marker) {
-            var properties = marker.properties,
-                toolTip = properties.grade || '';
-            if (toolTip && properties.elevation) {
-                toolTip += ', ';
-            }
-            toolTip += properties.elevation || '';
-            if (toolTip) {
-                toolTip = ' (' + toolTip + ')';
-            }
-            toolTip = (properties.name || 'без названия') + toolTip;
-            toolTip = (properties.is_summit ? 'Вершина ' : 'Перевал ') + toolTip;
-            return toolTip;
-        },
-
-        _passToGpx: function(marker) {
-            let label = marker.tooltip;
-            if (typeof label === 'function') {
-                label = label(marker);
-            }
-            label = escapeHtml(label);
-            const gpx = `<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+    passToGpx: function (marker) {
+        let label = marker.tooltip;
+        if (typeof label === 'function') {
+            label = label(marker);
+        }
+        label = escapeHtml(label);
+        const gpx = `<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
                 <gpx xmlns="http://www.topografix.com/GPX/1/1"
                      creator="http://nakarte.me"
                      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -95,17 +42,17 @@ const WestraPassesMarkers = L.Layer.CanvasMarkers.extend({
                     </wpt>
                 </gpx>
             `;
-            var filename = marker.properties.name || 'Без названия';
-            saveAs(new Blob([gpx], {type: 'application/gpx+xml'}), filename + '.gpx', true);
-        },
+        const filename = marker.properties.name || 'Без названия';
+        saveAs(new Blob([gpx], {type: 'application/gpx+xml'}), filename + '.gpx', true);
+    },
 
-        _passToKml: function(marker) {
-            let label = marker.tooltip;
-            if (typeof label === 'function') {
-                label = label(marker);
-            }
-            label = escapeHtml(label);
-            const kml = `<?xml version="1.0" encoding="UTF-8"?>
+    passToKml: function (marker) {
+        let label = marker.tooltip;
+        if (typeof label === 'function') {
+            label = label(marker);
+        }
+        label = escapeHtml(label);
+        const kml = `<?xml version="1.0" encoding="UTF-8"?>
                 <kml xmlns="http://www.opengis.net/kml/2.2">
                     <Placemark>
                         <name>${label}</name>
@@ -117,94 +64,73 @@ const WestraPassesMarkers = L.Layer.CanvasMarkers.extend({
                     </Placemark>
                 </kml>
             `;
-            var filename = marker.properties.name || 'Без названия';
-            saveAs(new Blob([kml], {type: 'application/vnd.google-earth.kml+xml'}), filename + '.kml', true);
-        },
+        const filename = marker.properties.name || 'Без названия';
+        saveAs(new Blob([kml], {type: 'application/vnd.google-earth.kml+xml'}), filename + '.kml', true);
+    },
 
-        _makeIcon: function(marker) {
-            let icon;
-            if (marker.properties.is_summit) {
-                icon = markerIcons.summit;
-            } else {
-                icon = markerIcons[marker.properties.grade_eng];
-            }
-            return icon;
-        },
+    makeIcon: function (marker) {
+        let icon;
+        if (marker.properties.is_summit) {
+            icon = markerIcons.summit;
+        } else {
+            icon = markerIcons[marker.properties.grade_eng];
+        }
+        return icon;
+    },
 
-        _loadMarkers: function(xhr) {
-            var markers = [],
-                features = JSON.parse(xhr.response),
-                feature, i, marker;
-            for (i = 0; i < features.length; i++) {
-                feature = features[i];
-                marker = {
-                    latlng: {
-                        lat: feature.latlon[0],
-                        lng: feature.latlon[1]
-                    },
-                    label: feature.name || "",
-                    icon: this._makeIcon,
-                    tooltip: this._makeTooltip.bind(this),
-                    properties: feature
-                };
-                markers.push(marker);
-            }
-            this.addMarkers(markers);
-            this._dataLoaded = true;
-            this.fire('data-loaded');
-        },
+    makePassName: function (passProperties) {
+        return passProperties.name || '';
+    },
 
-        showPassDescription: function(e) {
-            if (!this._map) {
-                return;
-            }
-            const properties = e.marker.properties,
-                latLng = e.marker.latlng,
-                url = 'https://westra.ru/passes/Passes/' + properties.id;
-            let altnames = '',
-                connects = '',
-                comments = '';
-            if (properties.altnames) {
-                altnames = `
+    makeDescription: function (marker) {
+        const properties = marker.properties;
+        const latLng = marker.latlng;
+        const url = 'https://westra.ru/passes/Passes/' + properties.id;
+        let altnames = '';
+        let connects = '';
+        let comments = '';
+        if (properties.altnames) {
+            altnames = `
                     <tr>
                         <td>Другие названия</td>
                         <td>${properties.altnames}</td>
                     </tr>`;
-            }
+        }
 
-            if (!properties.is_summit) {
-                connects = `
+        if (!properties.is_summit) {
+            connects = `
                     <tr>
                         <td>Соединяет</td>
-                        <td>${properties.connects || "неизвестнo"}</td>
+                        <td>${properties.connects || 'неизвестнo'}</td>
                     </tr>`;
-            }
+        }
 
-            if (properties.comments) {
-                for (let comment of properties.comments) {
-                    let user = '';
-                    if (comment.user) {
-                        user = `<span class="westra-passes-description-comment-author">${comment.user}:</span>`;
-                    }
-                    comments += `<p class="westra-passes-description-comment">${user}${comment.content}</p>`;
+        if (properties.comments) {
+            for (const comment of properties.comments) {
+                let user = '';
+                if (comment.user) {
+                    user = `<span class="westra-passes-description-comment-author">${comment.user}:</span>`;
                 }
-                comments = `
+                comments += `<p class="westra-passes-description-comment">${user}${comment.content}</p>`;
+            }
+            comments = `
                     <tr>
                         <td>Комментарии</td>
                         <td>${comments}</td>
                     </tr>`;
-            }
-            let reports;
-            if (properties.reports_total) {
-                reports =
-                    `<br>Отчетов: ${properties.reports_total}, ` +
-                    `с фото: ${properties.reports_photo || 0}, ` +
-                    `с описанием: ${properties.reports_tech || 0}`;
-            } else {
-                reports = '<br>Отчетов нет';
-            }
-            let description = `
-                <table class="pass-details">
+        }
+        let reports;
+        if (properties.reports_total) {
+            reports =
+                `<br>Отчетов: ${properties.reports_total}, ` +
+                `с фото: ${properties.reports_photo || 0}, ` +
+                `с описанием: ${properties.reports_tech || 0}`;
+        } else {
+            reports = '<br>Отчетов нет';
+        }
+        const description = L.DomUtil.create('div');
+        description.innerHTML = `
+                <table class="westra-pass-details">
                     <tr>
                         <td>${properties.is_summit ? 'Вершина ' : 'Перевал '}</td>
                         <td>${properties.name || 'название неизвестно'}</td>
@@ -212,16 +138,16 @@ const WestraPassesMarkers = L.Layer.CanvasMarkers.extend({
                     ${altnames}
                     <tr>
                         <td>Категория</td>
-                        <td>${properties.grade || "неизвестная"}</td>
+                        <td>${properties.grade || 'неизвестная'}</td>
                     </tr>
                     <tr>
                         <td>Высота</td>
-                        <td>${properties.elevation ? (properties.elevation + ' м') : 'неизвестная'}</td>
+                        <td>${properties.elevation ? properties.elevation + ' м' : 'неизвестная'}</td>
                     </tr>
                     ${connects}
                     <tr>
                         <td>Характеристика склонов</td>
-                        <td>${properties.slopes || "неизвестная"}</td>
+                        <td>${properties.slopes || 'неизвестная'}</td>
                     </tr>
                     <tr>
                         <td>Координаты</td>
@@ -245,25 +171,24 @@ const WestraPassesMarkers = L.Layer.CanvasMarkers.extend({
                         <td><a id="westra-pass-link" href="${url}">${url}</a>${reports}</td></tr>
                     <tr>
                         <td>Добавил</td>
-                        <td>${properties.author || "неизвестно"}</td>
+                        <td>${properties.author || 'неизвестно'}</td>
                     </tr>
                     ${comments}
                 </table>`;
-            this._map.openPopup(description, latLng, {maxWidth: 500});
-            document.getElementById('westra-pass-link').onclick = function() {
-                openPopupWindow(url, 780, 'westra-details');
-                return false;
-            };
-            document.getElementById('westra-pass-gpx').onclick = function() {
-                this._passToGpx(e.marker);
-                return false;
-            }.bind(this);
-            document.getElementById('westra-pass-kml').onclick = function() {
-                this._passToKml(e.marker);
-                return false;
-            }.bind(this);
-        }
-    }
-);
+        description.querySelector('#westra-pass-link').onclick = () => {
+            openPopupWindow(url, 780, 'westra-details');
+            return false;
+        };
+        description.querySelector('#westra-pass-gpx').onclick = () => {
+            this.passToGpx(marker);
+            return false;
+        };
+        description.querySelector('#westra-pass-kml').onclick = () => {
+            this.passToKml(marker);
+            return false;
+        };
+        return description;
+    },
+});
 
 export {WestraPassesMarkers};
