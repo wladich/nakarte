dc26d78345e9d7ed3f4d9f6d64f29b05a9657795
Sergej Orlov <wladimirych@gmail.com>
2025-01-14 22:40:24 +0100

    Store tracks and hash state in IndexedDB, add control for loading saved states
    fixes: #228

--- eslint_rules/imports_webapp.js
+++ eslint_rules/imports_webapp.js
@@ -14,7 +14,6 @@ const filesWithSideEffects = [
     'src/lib/leaflet.control.printPages/control.js',
     'src/lib/leaflet.control.track-list/control-ruler.js',
     'src/lib/leaflet.control.track-list/track-list.hash-state.js',
-    'src/lib/leaflet.control.track-list/track-list.localstorage.js',
     'src/lib/leaflet.hashState/Leaflet.Control.Layers.js',
     'src/lib/leaflet.hashState/Leaflet.Map.js',
     'src/lib/leaflet.hashState/leaflet.hashState.js',
